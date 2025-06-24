e4b5e1137a25172e9141467ba145915a3b1daa93
Bar Haim <barhaim2147@gmail.com>
2025-06-24 13:38:05 +0300

    🔥 ready to deploy with GitHub Actions

--- module/variables.tf
+++ module/variables.tf
@@ -1,78 +1,65 @@
 variable "vpc_cidr" {
-  description = "The CIDR block for the VPC"
-  type        = string
+  type = string
 }
 
 variable "public_subnet_cidr" {
-  description = "CIDR block for the first public subnet"
-  type        = string
+  type = string
 }
 
-variable "private_subnet_cidr" {
-  description = "CIDR block for the private subnet"
-  type        = string
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-}
-
-variable "ami_id" {
-  description = "AMI ID for EC2"
-  type        = string
-}
-
-variable "key_name" {
-  description = "SSH key pair name"
-  type        = string
-}
-
-variable "instance_name" {
-  description = "Name of the EC2 instance"
-  type        = string
-}
-
-variable "alb_name" {
-  description = "Name of the Application Load Balancer"
-  type        = string
-}
-
-variable "nat_name" {
-  description = "Name of the NAT Gateway"
-  type        = string
-}
-
-variable "sg_name" {
-  description = "Name for the EC2 security group"
-  type        = string
-}
-
-variable "vpc_name" {
-  description = "Name of the VPC"
-  type        = string
-}
-
-variable "tg_name" {
-  description = "Name of the Target Group"
-  type        = string
-}
-
-variable "ecr_image_uri" {
-  description = "The URI of the ECR image to pull for NGINX"
-  type        = string
-}
-
-variable "user_data" {
-  description = "The user data script to initialize the instance"
-  type        = string
-}
 variable "public_subnet_cidr_b" {
   type = string
 }
 
+variable "private_subnet_cidr" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "ami_id" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "instance_name" {
+  type = string
+}
+
+variable "alb_name" {
+  type = string
+}
+
+variable "nat_name" {
+  type = string
+}
+
+variable "sg_name" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "tg_name" {
+  type = string
+}
+
+variable "ecr_image_uri" {
+  type = string
+}
+
+variable "user_data" {
+  type = string
+}
+
 variable "private_key" {
-  description = "The private key for SSH"
+  description = "The private key used to connect to EC2 for provisioner"
   type        = string
   sensitive   = true
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,65 @@
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnet_cidr" {
+  type = string
+}
+
+variable "public_subnet_cidr_b" {
+  type = string
+}
+
+variable "private_subnet_cidr" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "ami_id" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "instance_name" {
+  type = string
+}
+
+variable "alb_name" {
+  type = string
+}
+
+variable "nat_name" {
+  type = string
+}
+
+variable "sg_name" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "tg_name" {
+  type = string
+}
+
+variable "ecr_image_uri" {
+  type = string
+}
+
+variable "user_data" {
+  type = string
+}
+
 variable "private_key" {
-  description = "The private key content for SSH"
+  description = "The private key used to SSH into EC2"
   type        = string
   sensitive   = true
 }
